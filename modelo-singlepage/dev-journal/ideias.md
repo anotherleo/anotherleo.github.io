049d7729f061f53207b6b1fda451db0c528656d9
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2021-04-21 06:01:21 -0300

    proj.

---
+++ modelo-singlepage/dev-journal/ideias.md
@@ -0,0 +1,5 @@
+* efeito de hover nos projetos que modifique as sombras.
+* efeito de troca de imagem no card de cada projeto.
+* pensar em algo criativo, mas minimalista, pra parte de habilidades.
+* colocar uma variável no circuit-bg para controlar a troca do modo claro para o modo escuro.
+* quem sabe alterar o bagulho do tema pra um switch de light/dark mode, tipo aquele botão daora do GitHub?
--- modelo-singlepage/script.js
+++ modelo-singlepage/script.js
@@ -3,8 +3,16 @@
 function startModal(modalID) {
     const modal = document.getElementById(modalID);
     modal.classList.add('show');
+
+    /* função para fechar ao clicar fora OU no botão OU no ícone de fechar da FontAwesome */
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'modal-close' || e.target.className == 'fas fa-times fa-lg') {
+            modal.classList.remove('show');
+        }
+    })
 }
 
+/* Variável de cada modal */
 const projectOne = document.getElementById('project-one');
 projectOne.addEventListener('click', function () {
     startModal('modal-project-one');
@@ -16,3 +24,6 @@ projectTwo.addEventListener('click', () => startModal('modal-project-two'));
 
 const projectThree = document.getElementById('project-three');
 projectThree.addEventListener('click', () => startModal('modal-project-three'));
+
+const projectFour = document.getElementById('project-four');
+projectFour.addEventListener('click', () => startModal('modal-project-four'));
